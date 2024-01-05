f9863f90d16b39ccc62127b25836d56fda335905
rayane.kanuka <rayane.kanuka@WJS-13>
2024-01-05 16:23:22 +0100

    TEST du MVVM

---
+++ OpenDataWPF/Commands/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace OpenDataWPF.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> _toExecute;
+        private Func<object, bool> _canExecute;
+
+        public RelayCommand(Action<object> toExcecute, Func<object, bool> canExecute = null)
+        {
+            _toExecute = toExcecute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+            {
+                return _canExecute(parameter);
+            }
+
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _toExecute(parameter);
+        }
+    }
+}
---
+++ OpenDataWPF/ViewModel/MetroViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OpenDataWPF.ViewModel
+{
+    public class MetroViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    private int _radius;
+    public int Radius
+    {
+        get { return _radius; }
+        set
+        {
+            _radius != value;
+            NotifyPropertyChanged("Radius");
+        }
+    }
+
+    public class MetroViewModel()
+    {
+        _radius = 400;
+    }
+
+    private void NotifyPropertyChanged(string name)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged (this,
+                new PropertyChangedEventHandler(name);
+        }
+    }
+
+}
